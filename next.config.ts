8abbc4f4de24915631659936398a446aff72632b
Brian Watkins <brian.watkins@gmail.com>
2025-04-17 17:22:15 -0400

    Copy prisma query engine explicitly during build

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
   },
-  outputFileTracingIncludes: {
-    "src/**/*": ["generated/**/*"],
-  },
 }
 
 export default nextConfig
