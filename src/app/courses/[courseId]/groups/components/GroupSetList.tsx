da364407f54754ee6a7a924683fb6eaa3ed28a5d
Brian Watkins <brian.watkins@gmail.com>
2025-04-17 10:39:18 -0400

    Use lucide icons on the group set component

--- src/app/courses/[courseId]/groups/components/GroupSetList.tsx
+++ src/app/courses/[courseId]/groups/components/GroupSetList.tsx
@@ -6,6 +6,7 @@ import GroupList from "./GroupList"
 import { DateTime } from "luxon"
 import { useGroupStore } from "@/app/contexts/GroupStoreContext"
 import GroupSetForm from "./GroupSetForm"
+import { Pencil, Trash2, PanelBottomClose, PanelBottomOpen } from "lucide-react"
 
 interface GroupSetProps {
   groupSet: DisplayableGroupSet
@@ -84,20 +85,7 @@ function GroupSet({ groupSet, expanded }: GroupSetProps) {
             className="mr-2 text-blue-600 hover:text-blue-800"
             aria-label="Edit group set"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-              />
-            </svg>
+            <Pencil className="w-5 h-5" />
           </button>
           <button
             data-delete-group-set-button
@@ -105,23 +93,17 @@ function GroupSet({ groupSet, expanded }: GroupSetProps) {
             className="mr-2 text-red-600 hover:text-red-800"
             aria-label="Delete group set"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
+            <Trash2 className="w-5 h-5" />
           </button>
-          <div className="text-blue-600 cursor-pointer" onClick={toggleExpand}>
-            {isExpanded ? "▲" : "▼"}
+          <div
+            className="text-blue-600 cursor-pointer hover:text-blue-800"
+            onClick={toggleExpand}
+          >
+            {isExpanded ? (
+              <PanelBottomOpen className="w-5 h-5" />
+            ) : (
+              <PanelBottomClose className="w-5 h-5" />
+            )}
           </div>
         </div>
       </div>
